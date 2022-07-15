a98bbdbf7c3ef8c46a6272d8b4bc566be3722837
Camilo Castro <development.camilo.castro@gmail.com>
2022-07-15 13:58:50 -0500

    Update the treesitter configuration for neovim

--- neovim/.configuration/nvim/lua/_treesitter.lua
+++ neovim/.configuration/nvim/lua/_treesitter.lua
@@ -1,30 +1,30 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-    ensure_installed = {
-        "css",
-        "dockerfile",
-        "go",
-        "hcl",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "python",
-        "sql",
-        "toml",
-        "typescript",
-        "yaml"
-    },
-    sync_install = false,
-    auto_install = true,
-    ignore_install = { "" },
-    highlight = {
-        enable = true,
-        disable = { "" },
-        additional_vim_regex_highlighting = false
-    }
+	ensure_installed = {
+		"bash",
+		"css",
+		"dockerfile",
+		"go",
+		"hcl",
+		"html",
+		"javascript",
+		"json",
+		"lua",
+		"make",
+		"markdown",
+		"python",
+		"sql",
+		"toml",
+		"typescript",
+		"yaml"
+	},
+	sync_install = false,
+	auto_install = true,
+	ignore_install = { "" },
+	highlight = {
+		enable = true,
+		disable = { "" },
+		additional_vim_regex_highlighting = false
+	}
 }
-
